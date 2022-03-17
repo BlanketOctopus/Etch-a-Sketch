0f2d95ef0df8584934256c6766b0aa07a4c97c44
BlanketOctopus <98364864+BlanketOctopus@users.noreply.github.com>
2022-03-18 10:32:00 +1300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+let columnValue = document.querySelector('#columnChoose');
+let rowValue = document.querySelector('#rowChoose');
 
+let container = document.querySelector('.container');
+container.addEventListener('mouseover', sketch);
+
+const start = document.querySelector('.start').addEventListener('click', function(){
+    let columns = columnValue.value;
+    let rows = rowValue.value;
+    document.querySelector('.start').style.display = 'none';
+    document.querySelector('.reset').style.display = 'flex';
+    createGrid(columns, rows);
+})
+
+const reset = document.querySelector('.reset').addEventListener('click', function(){
+    container.innerHTML = '';
+    let columns = columnValue.value;
+    let rows = rowValue.value;
+    createGrid(columns, rows);
+})
+
+function createGrid(columns, rows){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', columns);
+    for (i = 0; i < (rows * columns); i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('grid-item');
+    container.appendChild(cell);
+}}
+
+function sketch(event) {
+    if (event.target !== container) {
+        event.target.style.backgroundColor = `#000000`;
+    }
+}
